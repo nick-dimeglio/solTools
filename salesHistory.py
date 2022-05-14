abbef86c01242e1a86d41b8f3200bae54a94ad51
Nekolace <nickyd528@aol.com>
2022-05-14 18:11:33 -0400

    Added check for if the user inputs an invalid collection or howrare.is returns anything besides code 200

--- salesHistory.py
+++ salesHistory.py
@@ -36,33 +36,38 @@ def salesInformation(collection):
     url = f'https://api.howrare.is/v0.1/collections/{collection}'
     response = requests.get(url).json()
     # Filtering json to just get the items in the collection.
-    fullList = response['result']['data']['items']
-    # Looping through all the individual NFTs in the collection and each of their attributes to check for all possible attributes and adding them to the value array.
-    for item in fullList:
-        for attribute in item['attributes']:
-          #  if attribute['name'] not in attributeNames:
-           #     attributeNames.append(attribute['name'])
-            if str(attribute['value']).lower() not in attributeValues:
-                attributeValues.append(str(attribute['value']).lower())
-    # After successfully grabbing all possible valid attributes, asking user which they'd like to see the sales history of.
-    print("What attribute would you like to see the sales history of?")
-    # Gotta make sure its lowercase for simplicity!
-    desiredAttribute = str(input("Attribute: ").lower())
-    # A simple way to check if the user's input is among the valid attributes. If its not, just loop 'em through to choose again.
-    while desiredAttribute not in attributeValues:
-        if desiredAttribute in attributeValues:
-            break
-        print('That attribute does not exist in this collection. Please try another.')
+    validityCheck = response['result']
+    if validityCheck['api_code'] == 200:
+        fullList = response['result']['data']['items']
+        # Looping through all the individual NFTs in the collection and each of their attributes to check for all possible attributes and adding them to the value array.
+        for item in fullList:
+            for attribute in item['attributes']:
+                #  if attribute['name'] not in attributeNames:
+                #     attributeNames.append(attribute['name'])
+                if str(attribute['value']).lower() not in attributeValues:
+                    attributeValues.append(str(attribute['value']).lower())
+        # After successfully grabbing all possible valid attributes, asking user which they'd like to see the sales history of.
+        print("What attribute would you like to see the sales history of?")
+        # Gotta make sure its lowercase for simplicity!
         desiredAttribute = str(input("Attribute: ").lower())
-    # Once a valid attribute is chosen, loop through each NFT and its attribute once more to see if the NFT has the desired attribute
-    # If it does, add the token of the NFT to the desiredToken array.
-    for item in fullList:
-        for attribute in item['attributes']:
-            if str(attribute['value']).lower() == desiredAttribute:
-                desiredTokens.append(item['mint'])\
-                    # Once all the matches are found, send them to the saleCheck function to have their recent sale information checked.
-    print("NFT Tokens found. Finding recent sales history of all tokens. This may take a while depending on how many tokens grabbed and how steady MagicEden is at the moment...")
-    saleCheck(desiredTokens)
+        # A simple way to check if the user's input is among the valid attributes. If its not, just loop 'em through to choose again.
+        while desiredAttribute not in attributeValues:
+            if desiredAttribute in attributeValues:
+                break
+            print('That attribute does not exist in this collection. Please try another.')
+            desiredAttribute = str(input("Attribute: ").lower())
+        # Once a valid attribute is chosen, loop through each NFT and its attribute once more to see if the NFT has the desired attribute
+        # If it does, add the token of the NFT to the desiredToken array.
+        for item in fullList:
+            for attribute in item['attributes']:
+                if str(attribute['value']).lower() == desiredAttribute:
+                    desiredTokens.append(item['mint'])\
+                        # Once all the matches are found, send them to the saleCheck function to have their recent sale information checked.
+        print("NFT Tokens found. Finding recent sales history of all tokens. This may take a while depending on how many tokens grabbed and how steady MagicEden is at the moment...")
+        saleCheck(desiredTokens)
+    else:
+        print('There was an error grabbing this collection. Please make sure it was spelled correctly and try again.')
+        salesHistory()
 
 # FIRST STEP IN PROGRAM
 # Asks user for which collection they want to see the sales history of.
